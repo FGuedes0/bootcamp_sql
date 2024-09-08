e2992e309cec4d4e9c595332e1586ae98bdaceee
Fellipe Guedes <fellipe.guedes@outlook.com>
2024-09-08 13:31:39 -0300

    my first commit

---
+++ aula_01/primeiroscomandos.sql
@@ -0,0 +1,47 @@
+
+SELECT * FROM customers;
+
+SELECT CONTACT_NAME, CITY FROM customers;
+
+SELECT COUNTRY FROM customers;
+
+SELECT DISTINCT COUNTRY FROM customers;
+
+SELECT COUNT (COUNTRY) FROM customers;
+
+SELECT COUNT(DISTINCT COUNTRY) FROM CUSTOMERS;
+
+------------------------------------------------------------------
+
+SELECT * FROM customers WHERE country = 'Mexico';
+
+SELECT * FROM customers WHERE customer_id = 'ANATR';
+
+SELECT * FROM customers WHERE country = 'Germany' AND CITY = 'Berlin';
+
+SELECT * FROM customers WHERE city = 'Berlin' OR city = 'Aachen';
+
+SELECT * FROM customers WHERE country <> 'Germany';
+
+SELECT * FROM customers WHERE country = 'Germany' AND (city = 'Berlin' OR city = 'Aachen');
+
+SELECT * FROM customers WHERE country <> 'Germany' AND country <> 'USA';
+
+------------------------------------------------------------------
+
+SELECT * FROM customers ORDER BY country;
+
+SELECT * FROM customers ORDER BY country DESC;
+
+SELECT * FROM customers ORDER BY country, contact_name;
+
+SELECT * FROM customers ORDER BY country ASC, contact_name DESC;
+
+
+------------------------------------------------------------------
+
+SELECT * FROM customers WHERE contact_name LIKE 'A%' ORDER BY contact_name ASC;
+
+SELECT * FROM customers WHERE contact_name NOT LIKE 'A%' ORDER BY contact_name ASC;
+
+SELECT * FROM customers WHERE country IN ('Germany', 'France', 'UK');
